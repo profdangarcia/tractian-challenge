8950873a560516c6881b1d72004fa7484467f27d
Daniel Garcia <daniel.garcia@b2wdigital.com>
2024-12-02 11:00:53 -0300

    feat(home): estrutura da home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,24 @@
+import Image from "next/image";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Logo from "../../public/logo.png";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
   weight: "100 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
+
+const robotoRegular = localFont({
+  src: "./fonts/RobotoRegular.ttf",
+  variable: "--font-roboto",
   weight: "100 900",
 });
 
 export const metadata: Metadata = {
   title: "Tractian | Front-end Challenge",
-  description: "An Tree View Application that shows companies Assets",
+  description: "A Tree View Application that shows companies Assets",
 };
 
 export default function RootLayout({
@@ -26,8 +29,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${robotoRegular.variable} antialiased`}
       >
+        <header className="bg-secondary p-4">
+          <Image
+            src={Logo}
+            alt="Tractian logomark"
+            width={102.95}
+            height={14}
+          />
+        </header>
         {children}
       </body>
     </html>
